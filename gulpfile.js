33626e9e48a5e42cb1b68c4bb68a76964d0ef5a6
DaltonHart <daltonvaren@gmail.com>
2020-05-25 16:44:09 -0700

    set up gulp commands to start dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,34 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 
 function sassCompile() {
   return gulp
     .src("sass/**/*.scss")
     .pipe(sass())
-    .pipe(gulp.dest("public/styles"));
+    .pipe(gulp.dest("public/styles"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 }
 
-gulp.task("sass", sassCompile);
+function browserStart() {
+  return browserSync.init({
+    server: {
+      baseDir: "public",
+    },
+  });
+}
 
-gulp.task("watch", function () {
+function fileWatchers() {
   gulp.watch("sass/**/*.scss", sassCompile);
-});
+  gulp.watch("public/*.html", browserSync.reload);
+  gulp.watch("public/scripts/**/*.js", browserSync.reload);
+}
+
+gulp.task(
+  "default",
+  gulp.series(sassCompile, gulp.parallel(browserStart, fileWatchers))
+);
